9cd349adaa150b1d45b84cf82fbb3c710531a56c
LieriheartHuang <89817234+LieriheartHuang@users.noreply.github.com>
2021-09-01 10:26:40 -0700

    Update prep4.py

--- prep4.py
+++ prep4.py
@@ -1,5 +1,8 @@
-a=input("Number of the feet: ")
+a=int(input("Number of the feet: "))
+b=a*12
+c=a*0.3048
+d=1/16.5
 print(" ")
-print("Inches: "+a*12)
-print("Meter: "+a*0.3048)
-print("Rods: "+a/16.5)
+print("Inches: "+str(b))
+print("Meter: "+str(c))
+print("Rods: "+str(d))
